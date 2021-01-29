29881625baf46687d3474c753eb2f919863ca637
bushralam <bushralam606@gmail.com>
2021-01-29 13:55:37 +0530

    inital commit

---
+++ __tests__/TC01_basics.js
@@ -0,0 +1,25 @@
+describe('Post', () => {
+
+  beforeEach(async () => {
+    await page.goto('https://react-redux.realworld.io/#/login')
+  })
+  
+  afterEach(async () => {
+    page.close()
+  })
+  
+  test('Sign_In', async () => {
+    
+    const title = await page.title()
+    expect(title).toBe('Conduit')
+
+    await page.fill('input[type = "email"]', 'qacamp.acad@gmail.com')
+    await page.press('input[type = "email"]', 'Tab')
+    await page.type('input[type = "password"]', 'test12345')
+
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.click('form >> "Sign in"') // should pass
+    ])
+  })
+})
